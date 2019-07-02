04b3bd726be1f4f7e618b98e5844b424b91d248d
Jordan <48123323+JordanSOA@users.noreply.github.com>
2019-07-02 22:48:43 +0200

    adding donesection and a clear button

--- src/app/tasklist/tasklist.component.ts
+++ src/app/tasklist/tasklist.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Task } from './task';
-
-
+// import { Task } from './task';
+// import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-tasklist',
@@ -9,23 +8,33 @@ import { Task } from './task';
   styleUrls: ['./tasklist.component.css']
 })
 export class TasklistComponent implements OnInit {
-  Tasks = [];
+  Tasks: string[] = [];
   today: number = Date.now();
+  doneTasks: string[] = [];
 
-constructor() { }
-addTask(newTask: string) {
-  if (newTask) {
-    this.Tasks.push(newTask);
-    document.querySelector('form').reset();
-    console.log(this.today);
+  constructor() { }
+  addTask(newTask: string) {
+    if (newTask) {
+      this.Tasks.push(newTask);
+      document.querySelector('form').reset();
+    }
   }
-}
-// deleteAll(Tasks: string) {
-//   if (Tasks !== '') {
-//     document.querySelector('.todo').innerHTML = '';
-//   }
-// }
-ngOnInit() {
-}
+  deleteAll() {
+    if (this.Tasks.length > 0) {
+      this.Tasks.splice(0, this.Tasks.length);
+    }
+  }
+  // drop(event: CdkDragDrop<string[]>) {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  //   } else {
+  //     transferArrayItem(event.previousContainer.data,
+  //                       event.container.data,
+  //                       event.previousIndex,
+  //                       event.currentIndex);
+  ngOnInit() {
+  }
+  //   }
+  }
 
-}
+
